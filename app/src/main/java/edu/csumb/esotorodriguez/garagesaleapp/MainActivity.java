8b6dbcd031781dc2eb89d2c07df87f9c937e73c3
Eduardo445 <esoto-rodriguez@csumb.edu>
2020-11-13 18:10:03 -0800

    Fixing the project

---
+++ app/src/main/java/edu/csumb/esotorodriguez/garagesaleapp/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.csumb.esotorodriguez.garagesaleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
