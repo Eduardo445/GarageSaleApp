26bc86ec9c1a211d54b07c87b987f7b10ae701df
synkae <synkae123@gmail.com>
2020-11-18 16:55:39 -0800

    Added login capabilities

---
+++ app/src/main/java/edu/csumb/esotorodriguez/garagesaleapp/ParseApplication.java
@@ -0,0 +1,21 @@
+package edu.csumb.esotorodriguez.garagesaleapp;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //ParseObject.registerSubclass(Post.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("FxfjD8cDhRUX8TjYsvZbrbf83UrOHb5kL5nIrOmN")
+                .clientKey("pGrKMbrgriUFJLkqL3ZOMJGw8pxc1JP2btjmXXUO")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
