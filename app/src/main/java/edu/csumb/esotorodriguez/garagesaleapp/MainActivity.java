8dfd21220db4bd6ad8ee83b7c00df6a27dc9ed39
shawn <sdeppe@csumb.edu>
2020-11-20 19:31:21 +0900

    added post RecycleViewer

--- app/src/main/java/edu/csumb/esotorodriguez/garagesaleapp/MainActivity.java
+++ app/src/main/java/edu/csumb/esotorodriguez/garagesaleapp/MainActivity.java
@@ -1,14 +1,58 @@
 package edu.csumb.esotorodriguez.garagesaleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import edu.csumb.esotorodriguez.garagesaleapp.Adapters.Post;
+import edu.csumb.esotorodriguez.garagesaleapp.Adapters.PostAdapter;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    RecyclerView rvPosts;
+    List<Post> allPosts;
+    PostAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvPosts = findViewById(R.id.rvPost);
+
+        allPosts = new ArrayList<>();
+        adapter = new PostAdapter(this, allPosts);
+        rvPosts.setAdapter(adapter);
+        rvPosts.setLayoutManager(new LinearLayoutManager(this));
+        queryPosts();
+    }
+
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting post", e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "name: " + post.getKeyName() + ", location: " + post.getKeyLocation());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
